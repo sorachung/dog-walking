396dd76521f6c050a90bd2de9fb077a6e3058cae
sorachung <sorachung.src@gmail.com>
2021-11-22 15:12:05 -0600

    added event listener to show service area which clicked

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,17 +1,28 @@
-import { getWalkers } from "./database.js"
+import { getWalkers } from "./database.js";
 
-const walkers = getWalkers()
-
-
-export const Walkers = () => {
-    let walkerHTML = "<ul>"
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("walker")) {
+    const [, walkerId] = itemClicked.id.split("--");
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+      if (walker.id === parseInt(walkerId)) {
+        window.alert(`${walker.name} services ${walker.city}`);
+      }
     }
+  }
+});
 
-    walkerHTML += "</ul>"
+const walkers = getWalkers();
 
-    return walkerHTML
-}
+export const Walkers = () => {
+  let walkerHTML = "<ul>";
 
+  for (const walker of walkers) {
+    walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`;
+  }
+
+  walkerHTML += "</ul>";
+
+  return walkerHTML;
+};
